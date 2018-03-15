9ce6675c213f1e88b1743b6ba6b86b08880f4996
Theodoros Theodosiou <2theot10@solent.ac.uk>
2018-03-15 12:16:16 +0000

    AsyncTaskGetfiles

--- app/src/main/java/com/example/a2theot10/asynctaskandnetcomm/MainActivity.java
+++ app/src/main/java/com/example/a2theot10/asynctaskandnetcomm/MainActivity.java
@@ -13,11 +13,58 @@ import java.net.URL;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-
+import java.net.URLEncoder;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    class TestTask2 extends AsyncTask<String,Void,String>
+    {
+        public String doInBackground(String... params)
+        {
+            HttpURLConnection conn = null;
+            try
+            {
+                String artist = URLEncoder.encode(params[0], "UTF-8");
+
+
+                URL url = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php?artist="+artist);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStream in = conn.getInputStream();
+                if(conn.getResponseCode() == 200)
+                {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String result = "", line;
+                    while((line = br.readLine()) !=null)
+                    {
+                        result += line+"\n";
+                    }
+                    return result;
+                }
+                else
+                {
+                    return "HTTP ERROR: " + conn.getResponseCode();
+                }
+            }
+            catch(IOException e)
+            {
+                return e.toString();
+            }
+            finally
+            {
+                if(conn!=null)
+                {
+                    conn.disconnect();
+                }
+            }
+        }
+
+        public void onPostExecute(String result)
+        {
+            TextView tv = (TextView) findViewById(R.id.tv1);
+            tv.setText(result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +75,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     public void onClick(View view)
     {
-        TestTask2 task = new TestTask2(this);
+        TestTask2 task = new TestTask2();
         EditText et = (EditText)findViewById(R.id.et1);
-        TextView tv = (TextView)findViewById(R.id.tv1);
         String artist = et.getText().toString();
-        task.execute("http://www.free-map.org.uk/index.php");
+        task.execute(artist);
 
     }
 
